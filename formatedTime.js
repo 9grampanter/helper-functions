3cba5ed3cce7711aed9d104e680e1d328172eb50
9grampanter <zander2001@hotmail.se>
2022-03-22 17:08:12 +0100

    updated logic

--- formatedTime.js
+++ formatedTime.js
@@ -1,7 +1,9 @@
-const dubbleZero = parseInt('00');
-const time = new Date();
-const formatedDate = `
-  ${dubbleZero + time.getHours()}:
-  ${dubbleZero + time.getMinutes()}:
-  ${dubbleZero + time.getSeconds()}`;
-console.log(formatedDate);
+export default function formattedTime() {
+  const time = new Date();
+
+  const hours = time.getHours().toString().length > 1 ? time.getHours() : `0${time.getHours()}`
+  const minutes = time.getMinutes().toString().length > 1 ? time.getMinutes() : `0${time.getMinutes()}`
+  const seconds = time.getSeconds().toString().length > 1 ? time.getSeconds() : `0${time.getSeconds()}`
+  const formatedTime = `${hours}:${minutes}:${seconds}`;
+  return formatedTime;
+}
